61dceecee9a406017a9e55ab2b10cad16d1e916e
bilge-urcuk <bilgeurcukk@gmail.com>
2024-05-03 21:52:55 +0300

    gg

--- server/server.js
+++ server/server.js
@@ -28,5 +28,6 @@ app.use(bodyParser.json());
 
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}\n http://localhost:5050/`);
+  console.log(`Server listeninggg on port ${PORT}\n http://localhost:5050/`);
 });
+
