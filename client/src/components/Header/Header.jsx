62d5cc410f2317d2a175e0afb274203d08cdb2ea
Hasan Çoban <hasancoban13579@gmail.com>
2024-05-05 16:11:38 +0300

    Start using the useLocation hook to indicate the current page
    Using states to get the current url path causes selected button effects to disappear on page refresh.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,60 +1,50 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import NavItem from "./NavItem"
-import Icon from "../Icon/Icon"
-import Button from "../Button/Button"
-import SearchBar from "./SearchBar"
 import Logo from "../../../src/assets/logo2.png"
 import { useState } from "react"
 
 
 export default function Header() {
-  const [selectedItem, setSelectedItem] = useState('');
-
+  let pageName = useLocation().pathname;
 
   return (
     <div className="sticky w-full bg-white border-b font-semibold text-sm">
       <div className="max-w-7xl mx-auto">
         <div className="px-4 py-4">
           <div className="flex justify-between items-center">
-            <Link to={"/"} className="hover:cursor-pointer" onClick={() => {setSelectedItem('')}}>
+            <Link to={"/"} className="hover:cursor-pointer">
               <img src={Logo} alt="station watch logo" className="w-32" />
             </Link>
             <div className="w-full flex items-center gap-4 justify-center">
               <NavItem
                 name={"Test Oluştur"}
                 link={"/testolustur"}
-                onClick={() => setSelectedItem('Test Oluştur')}
-                style={selectedItem === 'Test Oluştur' ? 'selected' : ''}
+                style={/*selectedItem === 'Test Oluştur'*/ pageName === '/testolustur' ? 'selected' : ''}
               />
               <NavItem
                 name={"Bekleyen Testler"}
                 link={"/bekleyentestler"}
-                onClick={() => setSelectedItem('Bekleyen Testler')}
-                style={selectedItem === 'Bekleyen Testler' ? 'selected' : ''}
+                style={pageName === '/bekleyentestler' ? 'selected' : ''}
               />
               <NavItem
                 name={"Laboratuvarlar"}
                 link={"/laboratuvarlar"}
-                onClick={() => setSelectedItem('Laboratuvarlar')}
-                style={selectedItem === 'Laboratuvarlar' ? 'selected' : ''}
+                style={pageName === '/laboratuvarlar' ? 'selected' : ''}
               />
               <NavItem
                 name={"Test Geçmişi"}
                 link={"/testgecmisi"}
-                onClick={() => setSelectedItem('Test Geçmişi')}
-                style={selectedItem === 'Test Geçmişi' ? 'selected' : ''}
+                style={pageName === '/testgecmisi' ? 'selected' : ''}
               />
               <NavItem
                 name={"Bildirimler"}
                 link={"/bildirimler"}
-                onClick={() => setSelectedItem('Bildirimler')}
-                style={selectedItem === 'Bildirimler' ? 'selected' : ''}
+                style={pageName === '/bildirimler' ? 'selected' : ''}
               />
               <NavItem
                 name={"Ekle"}
                 link={"/ekle"}
-                onClick={() => setSelectedItem('Ekle')}
-                style={selectedItem === 'Ekle' ? 'selected' : ''}
+                style={pageName === '/ekle' ? 'selected' : ''}
               />
             </div>
             <div className="flex shrink-0 items-center justify-center w-12 h-12 border rounded-[50%] bg-sky-50 relative hover:cursor-pointer">
@@ -62,11 +52,7 @@ export default function Header() {
                 A
               </div>
             </div>
-
           </div>
-
-
-
         </div>
       </div>
     </div>
