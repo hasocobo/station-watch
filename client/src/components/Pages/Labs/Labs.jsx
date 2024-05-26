4c531f1b5a94a3153cd7a6d0b01b7311ebb03e6e
Hasan Çoban <hasancoban13579@gmail.com>
2024-05-27 01:23:40 +0300

    Create Labs page

--- client/src/components/Pages/Labs/Labs.jsx
+++ client/src/components/Pages/Labs/Labs.jsx
@@ -1,7 +1,80 @@
+import React from 'react'
+import StationCard from './StationCard'
+
+const stations = [
+  {
+    id: 1,
+    title: 'İstasyon 1',
+    cycles: '57',
+    status: 'AKTİF',
+    statusColor: 'bg-green-100',
+    textColor: 'text-green-800'
+  },
+  {
+    id: 2,
+    title: 'İstasyon 2',
+    cycles: '5',
+    status: 'AKTİF',
+    statusColor: 'bg-green-100',
+    textColor: 'text-green-800'
+  },
+  {
+    id: 3,
+    title: 'İstasyon 3',
+    cycles: '73',
+    status: 'DURDURULMUŞ',
+    statusColor: 'bg-yellow-100',
+    textColor: 'text-yellow-800',
+    componentId: 'ComponentId Değişimi'
+  },
+  {
+    id: 4,
+    title: 'İstasyon 4',
+    cycles: '',
+    status: 'BOŞ',
+    statusColor: 'bg-blue-100',
+    textColor: 'text-blue-800'
+  }
+]
+
 export default function Labs() {
   return (
-    <div>
-      <h1>Laboratuvarlar</h1>
+    <div className="mx-auto max-w-[80vw] bg-white p-6">
+      <div className="mb-6 flex items-center justify-between">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex gap-6">
+            <div className="text-lg text-slate-500">
+              <span className="font-bold text-slate-800">3</span> dolu istasyon
+            </div>
+            <div className="text-lg text-slate-500">
+              <span className="font-bold text-slate-800">1</span> boş istasyon
+            </div>
+          </div>
+          <div className="flex items-center rounded-lg border border-slate-300 hover:border-sky-300 bg-white p-1 shadow-sm">
+            <input
+              type="text"
+              placeholder="Ara"
+              className="pl-2 text-base outline-none"
+            />
+            <button className="ml-2 rounded-full px-2 py-2 text-slate-500 material-icons">
+              search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="grid mt-8 grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+        {stations.map((station) => (
+          <StationCard
+            key={station.id}
+            title={station.title}
+            cycles={station.cycles}
+            status={station.status}
+            statusColor={station.statusColor}
+            componentId={station.componentId}
+            textColor={station.textColor}
+          />
+        ))}
+      </div>
     </div>
   )
 }
---
+++ client/src/components/Pages/Labs/StationCard.jsx
@@ -0,0 +1,39 @@
+export default function StationCard({
+  title,
+  cycles,
+  status,
+  statusColor,
+  componentId,
+  textColor
+}) {
+  return (
+    <div className="rounded-md border bg-white shadow-md transition-shadow duration-200 hover:shadow-xl">
+      <div className=" bg-zinc-50 p-6 text-center text-xl font-semibold text-slate-700">
+        {title}
+      </div>
+      <div className="px-6 pb-6 pt-3">
+        {cycles && (
+          <div className="mb-4 flex items-center justify-center gap-1 text-center text-lg font-semibold text-slate-600">
+            <i className="material-symbols-outlined text-slate-500">cycle</i>
+            <div>
+              {cycles}.{' '}
+              <span className="text-base font-normal text-slate-500">
+                çevrim
+              </span>{' '}
+            </div>
+          </div>
+        )}
+        {componentId && (
+          <div className="mb-4 rounded-md bg-sky-100 p-2 text-center font-semibold text-sky-800">
+            {componentId}
+          </div>
+        )}
+        <div
+          className={`mt-auto rounded-md py-2 text-center font-semibold ${statusColor} ${textColor}`}
+        >
+          {status}
+        </div>{' '}
+      </div>
+    </div>
+  )
+}
